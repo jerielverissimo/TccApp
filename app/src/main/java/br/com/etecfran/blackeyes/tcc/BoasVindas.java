3952a6fb7985b2781dca4f0b9f133c0bbca4f844
monkeyTux <jeriel.verissimo@outlook.com>
2015-09-20 18:11:11 -0300

    Boas vindas

--- app/src/main/java/br/com/etecfran/blackeyes/tcc/BoasVindas.java
+++ app/src/main/java/br/com/etecfran/blackeyes/tcc/BoasVindas.java
@@ -1,33 +1,28 @@
 package br.com.etecfran.blackeyes.tcc;
 
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import android.os.Handler;
-
-public class SplashScreen extends AppCompatActivity implements Runnable{
+public class BoasVindas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_boas_vindas);
 
-        Handler SplashScreen = new Handler();
-        SplashScreen.postDelayed(SplashScreen.this, 3000);
+
     }
 
-    public void run(){
-        startActivity(new Intent(SplashScreen.this, BoasVindas.class));
-        finish();
-    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
+        getMenuInflater().inflate(R.menu.menu_boas_vindas, menu);
         return true;
     }
 
